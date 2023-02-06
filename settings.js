4c6c89270ee8a80124765caf602dfaecd1295917
Oumardow <113275771+Oumardow@users.noreply.github.com>
2023-02-06 17:04:39 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,15 +14,15 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94764497078'] //ur owner number
-global.ownername = "Sandaru Nethsara" //ur owner name
+global.owner = ['+223 67 12 89 92'] //ur owner number
+global.ownername = "MADARA UCHIWA" //ur owner name
 global.ytname = "YT: Sandaru Yt" //ur yt chanel name
 global.socialm = "GitHub: Mr-Katussa" //ur github or insta name
 global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
 
 //bot bomdy 
-global.ownernomer = "94764497078" //ur number
-global.premium = ['94764497078'] //ur premium number
+global.ownernomer = "+223 67 12 89 92" //ur number
+global.premium = ['+223 67 12 89 92'] //ur premium number
 global.botname = 'Xcoco' //ur bot name
 global.themeemoji = "♻️" //ur theme emoji
 global.packname = "Sticker By Xcoco" //ur sticker watermark packname
